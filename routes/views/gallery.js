5d15126c03d6a5602b0271fc41ad838e178e0775
Cazs <casperndlovu42@gmail.com>
2017-09-06 12:13:13 +0200

    Updated gallery page - added new image viewer.

--- routes/views/gallery.js
+++ routes/views/gallery.js
@@ -1,7 +1,8 @@
 var keystone = require('keystone');
+var fs = require('fs');
 
-exports = module.exports = function (req, res) {
-
+exports = module.exports = function (req, res)
+{
 	var view = new keystone.View(req, res);
 	var locals = res.locals;
 
@@ -11,7 +12,21 @@ exports = module.exports = function (req, res) {
 	// Load the galleries by sortOrder
 	view.query('galleries', keystone.list('Gallery').model.find().sort('sortOrder'));
 
-	// Render the view
-	view.render('gallery');
+	fs.readdir(__dirname + '/../../public/images/gallery', function(err, dir_data)
+	{
+		console.log('reading gallery images.\n');
+		if(err)
+		{
+			console.log(err);
+			res.status(500).send(err);
+			// Render the view
+			view.render('gallery');
+			return;
+		}
+		console.log('\n'+dir_data);
+		//view.query('imgs', dir_data);
+		// Render the view
+		view.render('gallery', {"images": dir_data});
+	});
 
 };
